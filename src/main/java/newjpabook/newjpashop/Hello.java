b26b2f7a95a0e06f66921de1c6f383f79260bad0
LEEYOSEPH <laksmi0628@gmail.com>
2022-07-21 08:20:27 +0900

    프로젝트 세팅

---
+++ src/main/java/newjpabook/newjpashop/Hello.java
@@ -0,0 +1,11 @@
+package newjpabook.newjpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+
+    private String data;
+
+}
--- src/main/java/newjpabook/newjpashop/NewjpashopApplication.java
+++ src/main/java/newjpabook/newjpashop/NewjpashopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewjpashopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(NewjpashopApplication.class, args);
 	}
 
