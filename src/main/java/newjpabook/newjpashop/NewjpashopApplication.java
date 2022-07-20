dd3d6e957a35033c2f3c45264e55bdbbda8d1f51
LEEYOSEPH <laksmi0628@gmail.com>
2022-07-21 08:14:58 +0900

    프로젝트 생성

---
+++ src/main/java/newjpabook/newjpashop/NewjpashopApplication.java
@@ -0,0 +1,13 @@
+package newjpabook.newjpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewjpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewjpashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/newjpabook/newjpashop/NewjpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package newjpabook.newjpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewjpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
