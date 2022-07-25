0666101403f5eeeaa474b765c62b903370022e5b
LEEYOSEPH <laksmi0628@gmail.com>
2022-07-26 08:23:55 +0900

    회원 서비스 개발

--- src/main/java/newjpabook/newjpashop/repository/MemberRepository.java
+++ src/main/java/newjpabook/newjpashop/repository/MemberRepository.java
@@ -1,17 +1,17 @@
 package newjpabook.newjpashop.repository;
 
+import lombok.RequiredArgsConstructor;
 import newjpabook.newjpashop.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
@@ -22,6 +22,12 @@ public class MemberRepository {
     }
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
                 .getResultList();
     }
 }
