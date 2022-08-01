8034f546f4c709590a59ffe118a0469ae629e10a
LEEYOSEPH <laksmi0628@gmail.com>
2022-08-02 07:50:28 +0900

    회원조회 V1: 응답 값으로 엔티티를 직접 외부에 노출

--- src/main/java/newjpabook/newjpashop/NewjpashopApplication.java
+++ src/main/java/newjpabook/newjpashop/NewjpashopApplication.java
@@ -1,7 +1,9 @@
 package newjpabook.newjpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NewjpashopApplication {
@@ -9,6 +11,11 @@ public class NewjpashopApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(NewjpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
 	}
 
 }
