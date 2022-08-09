02b810179ab351c75c6a5e6c4d314f457f705188
LEEYOSEPH <laksmi0628@gmail.com>
2022-08-10 08:12:34 +0900

    주문 조회 V1: 엔티티 직접 노출

---
+++ src/main/java/newjpabook/newjpashop/api/OrderApiController.java
@@ -0,0 +1,35 @@
+package newjpabook.newjpashop.api;
+
+import lombok.RequiredArgsConstructor;
+import newjpabook.newjpashop.domain.Order;
+import newjpabook.newjpashop.domain.OrderItem;
+import newjpabook.newjpashop.domain.OrderSearch;
+import newjpabook.newjpashop.repository.OrderRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기환
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName()); //Lazy 강제초기화
+        }
+        return all;
+    }
+}
